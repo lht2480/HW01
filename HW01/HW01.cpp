2d2aa5c872122e68fb920d34abe0988537c30f10
lht2480 <jgjjgiggk@gmail.com>
2025-05-27 15:31:33 +0900

    create HW01 project

---
+++ HW01/HW01.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double arr[5];
+
+    cout << "첫번째 학생 점수를 입력해주세요 : ";
+    cin >> arr[0];
+
+    cout << "두번째 학생 점수를 입력해주세요 : ";
+    cin >> arr[1];
+
+    cout << "세번째 학생 점수를 입력해주세요 : ";
+    cin >> arr[2];
+
+    cout << "네번째 학생 점수를 입력해주세요 : ";
+    cin >> arr[3];
+
+    cout << "다섯번째 학생 점수를 입력해주세요 : ";
+    cin >> arr[4];
+
+    double t = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
+    double a = t / 5;
+
+    cout << "총점 : " << t << endl;
+    cout << "평균 : " << a << endl;
+
+    return 0;
+}
